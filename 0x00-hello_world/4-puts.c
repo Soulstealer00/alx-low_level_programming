04ca449adf9a49327ae0a9b3302fa6517abce35b
Soulstealer <amineboxe11@gmail.com>
2023-08-15 05:26:35 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * Main -entry point
+ *
+ * Description : a program that runs a sentence using puts
+ *
+ * Return 0 = success
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
