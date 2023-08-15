de01f0a810a3ecf9d2edb17549ff2cb4160ed579
Soulstealer <amineboxe11@gmail.com>
2023-08-15 09:20:20 -0700

    Task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
 
+/**
+ * main - Entry
+ *
+ * Description: A C program that runs different sizes of variables
+ *
+ * Return: Always 0 (succes)
+*/
 
 int main(void)
-{       
-	printf("\nSize of a char: %lu byte(s)",sizeof(char));
-	printf("\nSize of an int: %lu byte(s)",sizeof(int));
-	printf("\nSize of a long int: %lu byte(s)",sizeof(long int));
-	printf("\nSize of a long long int: %lu byte(s)",sizeof(long long int)); 
-	printf("\nSize of a float: %lu byte(s)",sizeof(float));
+{
+	printf("\nSize of a char: %lu byte(s)", sizeof(char));
+	printf("\nSize of an int: %lu byte(s)", sizeof(int));
+	printf("\nSize of a long int: %lu byte(s)", sizeof(long int));
+	printf("\nSize of a long long int: %lu byte(s)", sizeof(long long int));
+	printf("\nSize of a float: %lu byte(s)", sizeof(float));
         return (0);
 }
