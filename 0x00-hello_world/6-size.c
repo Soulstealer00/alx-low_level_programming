290f2f43eb650643493a70aed1bb686176bd50e4
Soulstealer <amineboxe11@gmail.com>
2023-08-15 09:06:13 -0700

    Task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+
+int main(void)
+{       
+	printf("\nSize of a char : %lu Byte(s)",sizeof(char));
+	printf("\nSize of a int : %lu Byte(s)",sizeof(int));
+	printf("\nSize of long int : %lu Byte(s)",sizeof(long int));
+	printf("\nSize of long long int : %lu Byte(s)",sizeof(long long int)); 
+	printf("\nSize of float : %lu Byte(s)",sizeof(float));
+        return (0);
+}
