b611901b9f62fdbe51faecb71df18b4313f5c414
Soulstealer <amineboxe11@gmail.com>
2023-08-15 06:41:26 -0700

    Task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,11 +5,11 @@
   *
   * Description: A C program that runs a sentence using Printf
   *
-  * Return: Always 0 (success) 
+  * Return: Always 0 (success)
  */
 
 int main(void)
-{	
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
