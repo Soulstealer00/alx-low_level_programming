9d646c7592782d6b8b766027738fb38c4215f49a
Soulstealer <amineboxe11@gmail.com>
2023-08-15 05:08:33 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #!/bin/bash
-int main(){
-puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+ puts("\"Programming is like building a multilingual puzzle");
 return 0 ;
 }	
