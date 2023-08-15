1c6185bf2b88ad9de9a4b974d3d2431e58952feb
Soulstealer <amineboxe11@gmail.com>
2023-08-15 05:47:08 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,10 +8,8 @@
  * Return: Always 0 success
 */
 
-#include <stdio.h>
-
 int main(void)
 {
-         puts("\"Programming is like building a multilingual puzzle");
-         return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
