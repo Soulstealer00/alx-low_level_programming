ac8f2d2ca6353e06fb811c01370b859501149389
Soulstealer <amineboxe11@gmail.com>
2023-08-17 14:38:09 -0700

    task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include<unistd.h>
 
 /**
- * Main - Entry point
+ * main - Entry point
  *
  * Description:a c program that prints using write function
  *    ssize_t write(int fd,const void *buf,size_t count)
